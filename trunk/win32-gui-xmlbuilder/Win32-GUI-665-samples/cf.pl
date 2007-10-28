efb4fd2515ee6f1492e2dd73c656f878067aa443
blairuk <blairuk@ce5d6cce-913d-0410-bf2d-0f95086c823b>
2007-10-28 19:59:49 +0000

    Import from Home Repository

---
+++ trunk/win32-gui-xmlbuilder/Win32-GUI-665-samples/cf.pl
@@ -0,0 +1,42 @@
+
+
+use Win32::GUI;
+
+@ret = Win32::GUI::ChooseFont(
+    -name => "Courier New", 
+    -height => 76, 
+#    -size => 180,
+    -italic => 1,
+    -ttonly => 1,
+    -fixedonly => 1,
+    -script => 0,
+    -effects => 1,
+);
+
+if($#ret > 0) {
+    print "ChooseFont returned:\n";
+    %ret = @ret;
+    foreach $key (keys(%ret)) {
+        print "\t$key => $ret{$key}\n";
+    }
+
+	delete $ret{-size};
+
+    $F = new Win32::GUI::Font(%ret);
+
+    %ariret = $F->Info();
+
+    print "Info returned:\n";
+
+    foreach $key (keys(%ariret)) {
+        print "\t$key => $ariret{$key}\n";
+    }
+
+
+} else {
+    if(Win32::GUI::CommDlgExtendedError()) {
+        print "ERROR. CommDlgExtendedError is: ", Win32::GUI::CommDlgExtendedError(), "\n";    
+    } else {
+        print "You cancelled.\n";
+    }
+}
